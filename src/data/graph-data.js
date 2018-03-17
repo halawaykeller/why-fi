cc585962dd45412c94f2ab3b779553cd7b6468b4
kimberlykeller <keller.kimberly@gmail.com>
2018-03-17 21:56:10 +0000

    moved data working on inputing accurate data

---
+++ src/data/graph-data.js
@@ -0,0 +1,29 @@
+export const data = [
+      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+];
+
+export const data01 = [
+      {name: 'Page A', value: 2400, amt: 2400},
+      {name: 'Page B', value: 1398, amt: 2210},
+      {name: 'Page C', value: 9800, amt: 2290},
+      {name: 'Page D', value: 3908, amt: 2000},
+      {name: 'Page E', value: 4800, amt: 2181},
+      {name: 'Page F', value: 3800, amt: 2500},
+      {name: 'Page G', value: 4300, amt: 2100},
+];
+
+export const data02 = [
+      {name: 'Page A', value: 4000, amt: 2400},
+      {name: 'Page B', value: 3000, amt: 2210},
+      {name: 'Page C', value: 2000, amt: 2290},
+      {name: 'Page D', value: 2780, amt: 2000},
+      {name: 'Page E', value: 1890, amt: 2181},
+      {name: 'Page F', value: 2390, amt: 2500},
+      {name: 'Page G', value: 3490, amt: 2100},
+];
--- src/js/hook-graph.js
+++ src/js/hook-graph.js
@@ -2,37 +2,8 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 import styled from 'styled-components';
-import { LineChart, Line, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar, Pie, PieChart } from 'recharts';
-
-const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-];
-
-const data01 = [
-      {name: 'Page A', value: 2400, amt: 2400},
-      {name: 'Page B', value: 1398, amt: 2210},
-      {name: 'Page C', value: 9800, amt: 2290},
-      {name: 'Page D', value: 3908, amt: 2000},
-      {name: 'Page E', value: 4800, amt: 2181},
-      {name: 'Page F', value: 3800, amt: 2500},
-      {name: 'Page G', value: 4300, amt: 2100},
-];
-
-const data02 = [
-      {name: 'Page A', value: 4000, amt: 2400},
-      {name: 'Page B', value: 3000, amt: 2210},
-      {name: 'Page C', value: 2000, amt: 2290},
-      {name: 'Page D', value: 2780, amt: 2000},
-      {name: 'Page E', value: 1890, amt: 2181},
-      {name: 'Page F', value: 2390, amt: 2500},
-      {name: 'Page G', value: 3490, amt: 2100},
-];
+import { data, data01, data02 } from '../data/graph-data'
+import { LineChart, Line, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar, Pie, PieChart, Area, AreaChart } from 'recharts';
 
 // TODO : Need to pass appropriate, relvant data to these charts - pick a chart, any chart 
 // these are just example charts from http://recharts.org/#/en-US/examples
@@ -59,6 +30,14 @@ const HookGraph = (props) => (
           <Pie data={data01} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
           <Pie data={data02} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
         </PieChart>
+        
+        <AreaChart width={600} height={400} data={data} margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+            <XAxis dataKey="name"/>
+            <YAxis/>
+            <CartesianGrid strokeDasharray="3 3"/>
+            <Tooltip/>
+            <Area type='monotone' dataKey='uv' stroke='#8884d8' fill='#8884d8' />
+        </AreaChart>
     </React.Fragment>
 
 

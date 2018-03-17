0596da7faba0f83aa29ede8d604ef275bb0c6d1d
Katelyn <rosasynstylae@gmail.com>
2018-03-17 23:41:54 +0000

    Fix button import

--- src/js/donate-action.js
+++ src/js/donate-action.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Header} from 'semantic-ui-react';
-import { PrimaryButton as Button } from '../ui/button';
+import { PrimaryButton as Button } from './ui/button';
 
 /* SplashPageColumn:
  * A component that handles the styling for a column on the splash page

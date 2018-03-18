0aa3a794df356ae207b703d87a90134b09143e95
Erin <erinleeannscott@gmail.com>
2018-03-18 15:09:59 +0000

    updated hero image blurbs on homepage

--- src/js/by-line.js
+++ src/js/by-line.js
@@ -5,10 +5,10 @@ import styled from 'styled-components';
 const ByLine = (props) => (
     <React.Fragment>
         <h2 className={props.className}>
-           "Access to technology is critical to success in our world." 
+           Access to technology is critical to success in our fast-paced world. 
         </h2>
         <h5>
-            "Donate your laptop, smartphone, or tablet to someone in need"
+            Donate your laptop, smartphone, or tablet to create opportunity for someone in need.
         </h5>
     </React.Fragment>
     

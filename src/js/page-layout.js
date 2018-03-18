69144d795810b4c032be8f11318bc84696d7c828
Katelyn <rosasynstylae@gmail.com>
2018-03-18 16:48:46 +0000

    Work on header

--- src/js/page-layout.js
+++ src/js/page-layout.js
@@ -1,21 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import styled from 'styled-components';
 
 import { Menu } from 'semantic-ui-react';
 
+import { setCurrentPage } from '../data/actions';
+import { PAGES } from '../data/constants';
+
+const PageHeaderMenuItem = (props) => (
+    <Menu.Item {...props} />
+);
+
+const PageHeaderMenuItemStyled = styled(PageHeaderMenuItem)`
+    cursor: pointer;
+`;
+
 const PageHeaderMenu = (props) => (
     <Menu>
-        <Menu.Item>
-            Why-Fi
-        </Menu.Item>
+        <PageHeaderMenuItemStyled onClick={props.onHomeClick} content='Why-Fi' />
     </Menu>
 );
+
+const md2p = (dispatch) => ({
+    onHomeClick: () => {
+        dispatch(setCurrentPage(PAGES.HOME));
+    }
+});
+
+const PageHeaderMenuSmart = connect(null, md2p)(PageHeaderMenu);
 
 const PageLayout = (props) => {
     const { PageComponent, ...contentProps } = props;
     
     return (
         <React.Fragment>
-            <PageHeaderMenu />
+            <PageHeaderMenuSmart />
             <div className={props.className}>
                 <PageComponent {...contentProps} />
             </div>
